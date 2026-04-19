4a74eef3861339888426549813b0a3362223eebc
satetsu888 <satetsu888@gmail.com>
2026-04-19 14:58:02 +0900

    refactor: remove Colophon section and associated styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { Hero } from "./sections/Hero";
 import { About } from "./sections/About";
 import { Links } from "./sections/Links";
 import { Projects } from "./sections/Projects";
-import { Colophon } from "./sections/Colophon";
 
 export function App() {
   return (
@@ -11,7 +10,6 @@ export function App() {
       <About />
       <Links />
       <Projects />
-      <Colophon />
     </main>
   );
 }
